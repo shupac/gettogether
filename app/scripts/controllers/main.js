b15ef521f016d06175534edad919112d13fddb17
Hongxu Liu <djshu.us@gmail.com>
2013-10-30 11:11:16 -0700

    refactored map code to location service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -39,26 +39,7 @@ angular.module('GetTogetherApp')
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var getLocation = function() {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(displayMap);
-    } else {
-      alert("Browser doesn't support Geolocation");
-    }
-  };
-  var displayMap = function(position) {
-    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    displayContent(map, pos, 'Here');
-    map.setCenter(pos);
-  };
-  var displayContent = function(map, pos, content) {
-    new google.maps.InfoWindow({
-      map: map,
-      position: pos,
-      content: content
-    });
-  };
+  LocationService.getLocation(map);
 
   $scope.logout = function() {
     SessionService.logout();
--- app/scripts/services/location.js
+++ app/scripts/services/location.js
@@ -1,12 +1,26 @@
 angular.module('GetTogetherApp')
-.factory('LocationService', function($q){
+.factory('LocationService', function($http){
   var service = {
-    getLocation: function() {
-      var d = $q.defer();
-      navigator.geolocation.getCurrentPosition(function(position) {
-        d.resolve(position);
-      }, d.reject);
-      return d.promise;
+    getLocation: function(map) {
+      debugger;
+      service.map = map;
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(service.displayMap);
+      } else {
+        alert("Browser doesn't support Geolocation");
+      }
+    },
+    displayMap: function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      service.displayContent(service.map, pos, 'Here');
+      service.map.setCenter(pos);
+    },
+    displayContent: function(map, pos, content) {
+      new google.maps.InfoWindow({
+        map: service.map,
+        position: pos,
+        content: content
+      });
     }
   };
   return service;
